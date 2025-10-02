664bc64b4d33a67a0cf8a51e5b8f5ffc7211462b
peremenstudio <peremen.studio@gmail.com>
2025-10-02 11:09:06 +0300

    Add React app scaffold

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Basic GIS Platform</h1>
+      <p>Welcome to your GIS platform! The map and layers will appear here.</p>
+    </div>
+  );
+}
+
+export default App;
